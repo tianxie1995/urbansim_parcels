f124065f8a91324e7bec3844477bb01ef4ddc5d6
Paul Sohn <pksohn@gmail.com>
2017-03-23 17:05:24 -0700

    Remove custom code from core modules

--- urbansim_parcels/models.py
+++ urbansim_parcels/models.py
@@ -165,7 +165,7 @@ def residential_developer(feasibility, households, buildings, parcels, year,
         'residential_units',
         feasibility,
         parcels.parcel_size,
-        parcels.ave_sqft_per_unit_placeholder,
+        parcels.ave_sqft_per_unit,
         parcels.total_residential_units,
         'res_developer.yaml',
         year=year,
@@ -188,7 +188,7 @@ def non_residential_developer(feasibility, jobs, buildings, parcels, year,
         'job_spaces',
         feasibility,
         parcels.parcel_size,
-        parcels.ave_sqft_per_unit_placeholder,
+        parcels.ave_sqft_per_unit,
         parcels.total_job_spaces,
         'nonres_developer.yaml',
         year=year,
@@ -216,53 +216,3 @@ def scheduled_development_events(buildings, development_projects, summary,
             'jobs'])
 
     summary.add_parcel_output(new_buildings)
-
-
-@orca.step("diagnostic_output")
-def diagnostic_output(households, buildings, parcels, zones, year, summary):
-    households = households.to_frame()
-    buildings = buildings.to_frame()
-    parcels = parcels.to_frame()
-    zones = zones.to_frame()
-
-    zones['zoned_du'] = parcels.groupby('zone_id').zoned_du.sum()
-    zones['zoned_du_underbuild'] = (parcels.groupby('zone_id').
-                                    zoned_du_underbuild.sum())
-    zones['zoned_du_underbuild_ratio'] = (zones.zoned_du_underbuild /
-                                          zones.zoned_du)
-
-    zones['residential_units'] = (buildings.groupby('zone_id').
-                                  residential_units.sum())
-    zones['non_residential_sqft'] = (buildings.groupby('zone_id').
-                                     non_residential_sqft.sum())
-
-    zones['retail_sqft'] = (buildings.query('general_type == "Retail"').
-                            groupby('zone_id').non_residential_sqft.sum())
-    zones['office_sqft'] = (buildings.query('general_type == "Office"').
-                            groupby('zone_id').non_residential_sqft.sum())
-    zones['industrial_sqft'] = (
-        buildings.query('general_type == "Industrial"').
-            groupby('zone_id').non_residential_sqft.sum())
-
-    zones['average_income'] = households.groupby('zone_id').income.quantile()
-    zones['household_size'] = households.groupby('zone_id').persons.quantile()
-
-    zones['building_count'] = (buildings.
-                               query('general_type == "Residential"').
-                               groupby('zone_id').size())
-    zones['residential_price'] = (buildings.
-                                  query('general_type == "Residential"').
-                                  groupby('zone_id').
-                                  residential_price.quantile())
-    zones['retail_rent'] = (buildings[buildings.general_type == "Retail"].
-                            groupby('zone_id').
-                            non_residential_price.quantile())
-    zones['office_rent'] = (buildings[buildings.general_type == "Office"].
-                            groupby('zone_id').
-                            non_residential_price.quantile())
-    zones['industrial_rent'] = (buildings[
-                                    buildings.general_type == "Industrial"
-                                ].groupby('zone_id').
-                                non_residential_price.quantile())
-
-    summary.add_zone_output(zones, "diagnostic_outputs", year)
