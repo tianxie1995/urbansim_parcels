ae031ae49d42ced2cbe5e8f9ef4f46d2d3992b66
Tian Xie <xie.tim.tian@gmail.com>
2021-09-02 13:47:24 -0400

    Reverted part of the commit 39958b270bbe8b8cd15870a8fd033ed5634a7d31: Used all columns instead of local columns

--- urbansim_parcels/utils.py
+++ urbansim_parcels/utils.py
@@ -781,7 +781,7 @@ def run_feasibility(parcels, parcel_price_callback,
     pf = (sqftproforma.SqFtProForma.from_yaml(str_or_buffer=cfg)
           if cfg else sqftproforma.SqFtProForma.from_defaults())
     sites = (pl.remove_pipelined_sites(parcels) if pipeline
-             else parcels.local)
+             else parcels.to_frame())
     df = apply_parcel_callbacks(sites, parcel_price_callback,
                                 pf, **kwargs)
 
